1b901d2c679153e2d0c448cf91c74d5603485a92
Fanny Hoberg <fannyh.92@hotmail.com>
2023-12-28 14:19:55 +0100

    fixed function for adding products to cart with image, price, title. also fixed sum for each product in cart

--- frontend/src/add-to-cart.ts
+++ frontend/src/add-to-cart.ts
@@ -13,17 +13,67 @@ container.addEventListener("click", (e: MouseEvent) => {
     let product_id = Number(target.dataset.id);
     console.log("Detta är product_id", product_id);
 
-    // call function addToCart and send clicked products id
-    addToCart(product_id);
+    // get reference for closest product card
+    const parentProductEl = target.closest(".product-card") as HTMLElement;
+    console.log("Detta är parentProductEl; ", parentProductEl);
+
+    // get reference for clicked candy name
+    let candyNameToCart: string = "";
+
+    if (parentProductEl) {
+      const candyNameElement = parentProductEl.querySelector(
+        "#candy-name"
+      ) as HTMLHeadingElement;
+
+      // Check that candyNameElement is not null
+      if (candyNameElement) {
+        candyNameToCart = candyNameElement.textContent || "";
+        console.log("Detta är candyname:", candyNameToCart);
+      }
+    }
+    console.log("Detta är candyname:", candyNameToCart);
+
+    // get reference for clicked candy image source
+    let candyImageSrc: string = "";
+    if (parentProductEl) {
+      const candyImageElement = parentProductEl.querySelector(
+        "#candy-image"
+      ) as HTMLImageElement;
+
+      // Kontrollera att candyImageElement inte är null innan du fortsätter
+      if (candyImageElement) {
+        candyImageSrc = candyImageElement.getAttribute("src") || "";
+
+        console.log("Detta är candyImageSrc:", candyImageSrc);
+      }
+    }
+    console.log("Detta är candyImageSrc:", candyImageSrc);
+
+    // get reference for clicked candy price
+    let candyPriceToCart: string = "";
+
+    if (parentProductEl) {
+      const candyPriceElement = parentProductEl.querySelector(
+        "#candy-price"
+      ) as HTMLElement;
+
+      // Check that candyNameElement is not null
+      if (candyPriceElement) {
+        candyPriceToCart = candyPriceElement.textContent || "";
+        console.log("Detta är candyPriceToCart:", candyPriceToCart);
+      }
+    }
+    console.log("Detta är candyPriceToCart:", candyPriceToCart);
+
+    // call function addToCart with value of clicked candy
+    addToCart(product_id, candyNameToCart, candyImageSrc, candyPriceToCart);
+
+    // count for clicks and display next to cart
     totalAmount++;
     if (totalAmount > 0) {
       totalClicksEl.innerHTML = `<p>${totalAmount}</p>`;
     }
   }
-
-  const parentProductEl = target.closest(".product-card") as HTMLElement;
-
-  console.log("Detta är parentProductEl; ", parentProductEl);
 
   // Need to save data to Local Storage with every click on button
 });
@@ -34,13 +84,24 @@ let carts: Product[] = [];
 const cartlistEL = document.querySelector<HTMLElement>("#cart-list")!;
 const cartEl = document.querySelector<HTMLElement>("#cart")!;
 
-// function to add product to cart with id and quantity
-const addToCart = (product_id: number) => {
+// function to add product to cart array and update quantity
+const addToCart = (
+  product_id: number,
+  candyNameToCart: string,
+  candyImageSrc: string,
+  candyPriceToCart: string
+) => {
+  console.log("Detta är candyname i addTocart:", candyNameToCart);
+  console.log("Detta är product_id i addTocart:", product_id);
+
   let productInCart = carts.findIndex((value) => value.id == product_id);
   // checks if cart is empty, then fill cart with info
   if (carts.length <= 0) {
     carts = [
       {
+        price: candyPriceToCart,
+        images: candyImageSrc,
+        name: candyNameToCart,
         id: product_id,
         stock_quantity: 1,
       },
@@ -48,6 +109,9 @@ const addToCart = (product_id: number) => {
     // checks if productInCart does not exists in cart, then push to cart
   } else if (productInCart < 0) {
     carts.push({
+      price: candyPriceToCart,
+      images: candyImageSrc,
+      name: candyNameToCart,
       id: product_id,
       stock_quantity: 1,
     });
@@ -56,6 +120,7 @@ const addToCart = (product_id: number) => {
     carts[productInCart].stock_quantity =
       carts[productInCart].stock_quantity + 1;
   }
+  //call function to render to cart
   addToCartRender();
   console.log("detta är cartlist : ", cartEl);
 };
@@ -63,6 +128,7 @@ const addToCart = (product_id: number) => {
 // render products to shoppingcart
 const addToCartRender = () => {
   cartlistEL.innerHTML = ``;
+
   if (carts.length > 0) {
     cartEl.classList.remove("hide");
 
@@ -74,19 +140,19 @@ const addToCartRender = () => {
     carts.forEach((cart) => {
       console.log("detta är carts i for each: ", carts);
       let newItemInCart = document.createElement("li");
+      let priceProduct = cart.price * cart.stock_quantity;
       newItemInCart.classList.add("list-item");
       newItemInCart.innerHTML = `
       <div class="list-item-content">
         <img
           class="cart-thumbnail"
-          src="https://www.bortakvall.se/storage/products/thumbnails/1997509-300x300.png"
+          src="${cart.images}"
           alt="Product thumbnail"
         />
         <div class="list-text-content">
-          <p class="cart-item-title">Gott & Blandat Giants</p>
-          <p class="cart-item-title">Id: ${cart.id}</p>
-          <p class="cart-item-price">Antal: ${cart.stock_quantity}</p>
-          <p class="cart-item-price">24 kronor</p>
+          <p class="cart-item-title">${cart.name}</p>
+          <p class="cart-item-price">Antal: ${cart.stock_quantity} st</p>
+          <p class="cart-item-price">Summa: ${priceProduct} kr</p>
         </div>
       </div>
       <button class="remove-item">
@@ -94,10 +160,10 @@ const addToCartRender = () => {
       </button>
 `;
       cartlistEL.appendChild(newItemInCart);
-      // console.log("detta är cartlist : ", cartlistEL);
     });
   }
 };
+
 const cartWrapperEl = document.querySelector<HTMLElement>("#cart-wrapper")!;
 
 const openCartEl = document.querySelector<HTMLElement>("#open-cart")!;
